ad3cf52742ef3f529b03c7cd6b27229794d5cddd
Amatul Sharin <sharinamatul@gmail.com>
2022-04-19 21:11:22 +0700

    polishing the look. haven't set onPageChanged func

--- lib/main.dart
+++ lib/main.dart
@@ -43,74 +43,126 @@ class _MyHomePageState extends State<MyHomePage> {
         image: 'assets/images/Screenshot 2022-04-19 061103.png'),
   ];
 
+  Color componentColor = Color.fromARGB(255, 255, 183, 116);
+  Color fontColor = Color.fromARGB(255, 61, 61, 61);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //Set the background color of Scaffold (the overall screen)
-      backgroundColor: const Color(0xFFFFE8C7),
+        //Set the background color of Scaffold (the overall screen)
+        backgroundColor: const Color(0xFFFFE8C7),
 
-      //Put all of the elements inside the container, so that I can
-      //use "decoration" property
-      body: Container(
-        //Organize the contents in column
-        child: Column(
-          children: [
-            //Use the Padding() class to wrap the title
-            const Padding(
-              padding: EdgeInsets.fromLTRB(16, 30, 16, 25),
-              child: Text(
-                "Aksara\nNusantara",
-                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 60),
-                maxLines: 2,
-              ),
-            ),
+        //Put all of the elements inside the container, so that I can
+        //use "decoration" property
+        body: SafeArea(
+          child: Container(
+            //padding: EdgeInsets.only(top: ),
+            //Organize the contents in column
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(16, 30, 16, 25),
+                    child: const Text(
+                      "Aksara\nNusantara.",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 48,
+                          fontFamily: 'InterBold'),
+                      maxLines: 2,
+                    ),
+                  ),
+                ),
 
-            //   //Add CarouselSlider
-            CarouselSlider(
-              options: CarouselOptions(height: 200.0),
-              items: aksara.map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
+                //const Spacer(),
+
+                //   //Add CarouselSlider
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 270.0,
+                    enlargeCenterPage: true,
+                    viewportFraction: 0.70,
+                  ),
+                  items: aksara.map((i) {
+                    // return Builder(
+                    //   builder: (BuildContext context) {
                     return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(color: Colors.amber),
+                        width: /*  240, */ MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                            color: componentColor,
+                            borderRadius: BorderRadius.circular(15.0)),
                         child: Column(
                           children: [
-                            Image.asset(
-                              '${i.image}',
-                              height: 100.0,
-                            ),
-                            Text(
-                              '${i.name}',
-                              style: TextStyle(
-                                fontSize: 40,
-                                fontWeight: FontWeight.w800,
+                            Container(
+                              padding: EdgeInsets.all(16),
+                              child: Image.asset(
+                                '${i.image}',
+                                height: 140.0,
                               ),
-                              maxLines: 2,
                             ),
-                            Text('${i.desc}')
+                            //Spacer(),
+                            Expanded(
+                              flex: 1,
+                              child: Text(
+                                '${i.name}',
+                                style: TextStyle(
+                                  fontFamily: 'InterSemiBold',
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w800,
+                                  color: fontColor,
+                                ),
+                                textAlign: TextAlign.center,
+                                maxLines: 2,
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Text(
+                                '${i.desc}',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 102, 102, 102),
+                                    fontFamily: 'Inter',
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 16),
+                              ),
+                            ),
                           ],
                         ));
-                  },
-                );
-              }).toList(),
+                    //},
+                    //);
+                  }).toList(),
+                ),
+
+                //Add spacer between the button and the slider
+                //const Spacer(),
+                const Spacer(
+                  flex: 2,
+                ),
+
+                Container(
+                  width: double.infinity,
+                  height: 60,
+                  margin: EdgeInsets.all(16.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Lebih lanjut",
+                      style: TextStyle(
+                          fontFamily: 'InterBold',
+                          fontSize: 20,
+                          color: fontColor),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(componentColor)),
+                  ),
+                )
+              ],
             ),
-            Container(
-              width: double.infinity,
-              height: 80,
-              margin: EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Lebih lanjut"),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.amber)),
-              ),
-            )
-          ],
-        ),
-        decoration: const BoxDecoration(),
-      ),
-    );
+            decoration: const BoxDecoration(),
+          ),
+        ));
   }
 }
